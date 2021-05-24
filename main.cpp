3592c397e54936ea10450b2ddc3c284cb5b0b467
Miqueias Araujo <>
2021-05-24 02:32:40 +0000

    //retorna o maior elemento do vetor

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  int maxVet(int *vet, int tam){
+    if (tam == 1)
+    return vet[0];
+    else{
+      int aux;
+      aux = maxVet (vet,tam - 1);
+      if (aux > vet[tam - 1])
+      return aux;
+      else
+      return (vet[tam-1]);
+    }
+  }
+
+
 }
